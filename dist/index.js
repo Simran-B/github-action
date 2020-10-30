9930de570b478e10b12a5bcd3c1c378e54520b4d
Simran Spiller <simran.spiller@gmail.com>
2020-10-30 01:02:53 +0100

    Add output repeated

--- dist/index.js
+++ dist/index.js
@@ -11,6 +11,7 @@ async function run() {
     try {
         const str = core.getInput("string")
         core.setOutput("reversed", str.split("").reverse().join(""))
+        core.setOutput("repeated", str.repeat(2))
     } catch (error) {
         core.setFailed(error.message)
     }
--- index.test.js
+++ index.test.js
@@ -7,6 +7,8 @@ test("valid string", () => {
     process.env["INPUT_STRING"] = "Hello World!"
     const ip = path.join(__dirname, "index.js")
     const stdout = cp.execSync(`node ${ip}`, {env: process.env}).toString()
-    expect(stdout.trim()).toBe("::set-output name=reversed::!dlroW olleH")
+    const lines = stdout.split("\n")
+    expect(lines[0].trim()).toBe("::set-output name=reversed::!dlroW olleH")
+    expect(lines[1].trim()).toBe("::set-output name=repeated::Hello World!Hello World!")
     console.log(stdout)
 })
